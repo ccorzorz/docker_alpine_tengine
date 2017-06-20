0f81b71483ea0ec61917006cd29e2395f6bcf722
Shane <ccniubi@163.com>
2017-06-20 11:43:11 +0800

    修复bug,不挂载目录无法启动的问题

--- script/start.sh
+++ script/start.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 set -e
 
+FILE="/usr/local/nginx/conf/mime.types"
 count=`ls /usr/local/nginx/conf | wc -l`
+echo `ls /usr/local/nginx/conf`
+echo "+++++++++++++"
+echo `ls /mnt/conf`
 if [[ "$count" = "0" ]];then
 	echo 'the nginx conf is empty!'
 	mv /mnt/conf/* /usr/local/nginx/conf/	
 	rm -rf /mnt/conf
-else
+fi
+
+if [[ ! -f "$FILE" ]];then
+	cp -rf /mnt/conf/* /usr/local/nginx/conf/
+	echo `ls /usr/local/nginx/conf`
 	rm -rf /mnt/conf
 fi
 exec /usr/local/nginx/sbin/nginx -g "daemon off;"
